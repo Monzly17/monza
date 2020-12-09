30b23829c5a01d5e5c7dedc463848d9ac8353713
MonzLy <75698415+Monzly17@users.noreply.github.com>
2020-12-09 20:20:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
 const BotName = 'MONZLY BOT'; 
-const instagram = 'LINK INSTAGRAMU'; 
-const telegram = 'LINK TELEMU'; 
+const instagram = 'https://www.instagram.com/fdy_moonza'; 
+const telegram = 'https://t.me/Monzly'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'LINK YTMU';
-//const grupch1 = 'belum ada grup'; 
+const youtube = 'https://www.youtube.com/channel/UCEPzMNcqhU6jaIPHj5MCHHg';
+//const grupch1 = 'https://chat.whatsapp.com/EX7T4wSrfhLJDAvTSoCeSe'; 
 //const grupch2 = 'belum ada grup' ; 
 const
 {
@@ -64,7 +64,7 @@ fs.existsSync('./session.json') && conn.loadAuthInfo('./session.json')
 //conn.connectOptions.agent = ProxyAgent ('http://1.0.180.120:8080')
 conn.connect();
 
-conn.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log('Bot by ig:@reuz.id')
+conn.on('user-presence-update', json => console.log(json.id + ' presence is => ' + json.type)) || console.log('Bot by ig:@fdy_moonza.id')
 conn.on('message-status-update', json =>
 {
    const participant = json.participant ? ' (' + json.participant + ')' : '' // participant exists when the message is from a group
@@ -288,7 +288,7 @@ switch(bulan) {
 }
 var tampilTanggal = "TANGGAL: " + hari + ", " + tanggal + " " + bulan + " " + tahun;
 var tampilWaktu = "JAM: " + jam + ":" + menit + ":" + detik;
-conn.sendMessage(id, menu.menu(id, BotName, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, youtube, kapanbotaktif) ,MessageType.text);
+conn.sendMessage(id, menu.menu(id, Botname, corohelp, tampilTanggal, tampilWaktu, instagram, telegram, youtube, kapanbotaktif) ,MessageType.text);
 }
 else if (text == '!quran'){
 axios.get('https://api.banghasan.com/quran/format/json/acak').then((res) => {
